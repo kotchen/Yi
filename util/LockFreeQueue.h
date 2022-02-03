4be6e57395d5c89cbf52cd1dbb95dee45f5b7f6f
kotchen <549642010@qq.com>
2022-02-03 17:43:53 +0800

    实现了无锁队列

--- util/LockFreeQueue.h
+++ util/LockFreeQueue.h
@@ -1,155 +1,81 @@
-﻿#ifndef __LCOK_FREE_QUEUE__
-#define __LCOK_FREE_QUEUE__
-#include <queue>
+﻿#ifndef __YI_LOCKFREEQUEUE__
+#define __YI_LCOKFREEQUEUE__
+
 #include <atomic>
+#include <thread>
+#include <vector>
+#include <cstring>
+#include <string>
 #include <iostream>
-#include <condition_variable>
-#include "TLSF.hpp"
+#include <deque>
 
-template<typename ValueType>
-struct Node;
-
-template<typename ValueType>
-struct Pointer
+namespace yi
 {
-    Node<ValueType>* ptr;
-    uint32_t count;
-    bool operator== (const Pointer<ValueType>& rhs)
-    {
-        return (ptr == rhs.ptr) && (count == rhs.count);
-    }
-};
 
-template<typename ValueType>
-struct Node
-{
-    ValueType _data;
-    std::atomic<Pointer<ValueType>> _next;
-    Node () : _data(ValueType()), _next({nullptr, 0})
+    template <typename ValueType>
+    class LockFreeQueue
     {
-    }
-    Node (ValueType data) : _data(data), _next({nullptr, 0})
-    {
-    }
-    Node (const Node<ValueType>&) = delete;
-    Node& operator= (const Node<ValueType>&) = delete;
-};
-template<typename ValueType>
-struct PopReturn
-{
-    ValueType _ret;
-    bool      _state;
-};
-
-template<typename ValueType>
-class LockFreeQueue
-{
-private:
-    /* data */
-    std::atomic<Pointer<ValueType>> _head;
-    std::atomic<Pointer<ValueType>> _tail;
-
-    // std::shared_ptr<TLSF>           _tlsf;
-    void destruct_helper(Pointer<ValueType> pointer)
-    {
-        if (pointer.ptr)
+    private:
+        static constexpr size_t CACHELINE_ALIGNMENT = 64 - sizeof(size_t);
+        struct Node
         {
-            auto next = pointer.ptr->_next.load(std::memory_order_relaxed);
-            delete  pointer.ptr; 
-            destruct_helper(next);
+            ValueType _data;
+            bool _used;
+        };
+
+    public:
+        size_t _capacity;
+        Node *_node_head;
+        std::atomic<size_t> _head;
+        char _cacheline_alignment[CACHELINE_ALIGNMENT];
+        std::atomic<size_t> _tail;
+        LockFreeQueue(size_t queue_size)
+            : _capacity(queue_size),
+              _head(0),
+              _tail(0),
+              _cacheline_alignment{0}
+        {
+            _node_head = ::new Node[_capacity];
+            for (int i = 0; i < _capacity; i++)
+                _node_head[i]._used = false;
         }
-    }
-    void print_helper(Pointer<ValueType> pointer)
-    {
-        if (pointer.ptr)
+        ~LockFreeQueue()
         {
-            std::cout << pointer.ptr->_data << ' ';
-            print_helper(pointer.ptr->_next);
+            delete[] _node_head;
         }
 
-    }
-public:
-    // LockFreeQueue(std::shared_ptr<TLSF> tlsf) : _tlsf(tlsf)
-    LockFreeQueue()
-    {
-        auto node = new Node<ValueType> ();
-        _head = {node, 0};
-        _tail = {node, 0};
-    }
-    ~LockFreeQueue()
-    {
-        destruct_helper(_head.load(std::memory_order_relaxed));
-    }
-
-    void Push(const ValueType& data)
-    {
-        Node<ValueType>* node = new Node<ValueType> (data);
-        Pointer<ValueType> tail;
-        Pointer<ValueType> next;
-        while (true)
+        bool Push(const ValueType &data)
         {
-            tail = _tail.load();
-            next = tail.ptr->_next;
-            if (tail == _tail.load(std::memory_order_relaxed))
+            while (true)
             {
-                if (!next.ptr)
+                auto tail = _tail.load();
+                if (_node_head[tail]._used)
+                    return false;
+                if (_tail.compare_exchange_weak(tail, (tail + 1) % _capacity))
                 {
-                    if (tail.ptr->_next.compare_exchange_weak(next, {node, next.count+1}, std::memory_order_relaxed))
-                        break;
-                }
-                else    
-                    _tail.compare_exchange_weak(tail, {next.ptr, tail.count+1}, std::memory_order_relaxed);
-            }
-
-        }
-        _tail.compare_exchange_weak(tail, {node, tail.count+1}, std::memory_order_relaxed);
-    }
-
-    PopReturn<ValueType> Pop()
-    {
-        PopReturn<ValueType> ret;
-        Pointer<ValueType> head;
-        Pointer<ValueType> tail;
-        while (true)
-        {
-            head = _head.load(std::memory_order_relaxed);
-            tail = _tail.load();
-            // auto next = head->
-            Pointer<ValueType> next = {head.ptr->_next.load(std::memory_order_relaxed).ptr, 0};
-            if (head == _head.load(std::memory_order_relaxed))
-            {
-                if (head.ptr == tail.ptr)
-                {
-                    if (!next.ptr)
-                        // empty
-                    {
-                        ret._state = false;
-                        return ret;
-                    }
-                    else
-                        _tail.compare_exchange_weak(tail, {next.ptr, tail.count+1}, std::memory_order_relaxed);
-                }
-                else
-                {
-                    ret._ret = next.ptr->_data;
-                    if (_head.compare_exchange_weak(head, {next.ptr, head.count+1}, std::memory_order_relaxed))
-                        break;
+                    _node_head[tail]._used = true;
+                    _node_head[tail]._data = data;
+                    return true;
                 }
             }
         }
-        delete head.ptr;
-        ret._state = true;
-        return ret;
-    }
 
-    void print()
-    {
-        Pointer<ValueType> pointer = _head.load(std::memory_order_relaxed);
-        print_helper(pointer.ptr->_next);
-    }
+        bool Pop(ValueType &ret)
+        {
+            while (true)
+            {
+                auto head = _head.load();
+                if (!_node_head[head]._used)
+                    return false;
+                if (_head.compare_exchange_weak(head, (head + 1) % _capacity))
+                {
+                    _node_head[head]._used = false;
+                    ret = _node_head[head]._data;
+                    return true;
+                }
+            }
+        }
+    };
+
 };
-
-
-
-
 #endif
