a0600fce6eaf04a88a0c3071afe48d48367892ba
kotchen <549642010@qq.com>
2022-04-09 22:06:44 +0800

    新增单例模式

--- util/yi_type.h
+++ util/yi_type.h
@@ -1,8 +1,34 @@
-﻿#include <functional>
+﻿#ifndef __YI_TYPE_DESIGN__
+#define __YI_TYPE_DESIGN__
+
+#include <functional>
 #include <vector>
 #include <any>
+#include <tuple>
 // that function we write to call, such as add
+namespace yi
+{
 using Function = std::function<std::vector<std::any>(std::vector<std::any>)>;
 
-using FunctionRet = std::vector<std::any>;
-using FunctionParams = std::vector<std::any>;
+// string : function  name
+// vector ： parameters list
+using FunctionCallInfo = std::tuple<std::string, std::vector<std::any>>;
+
+using ConnectCallBack = std::function<void()>;
+
+using FunctionCall_CallBack = std::function<std::vector<std::any>(std::vector<std::any>)>;
+
+enum CallType
+{
+    CONNECTION = 0,
+    DISCONNECTION = 1,
+
+    FUNCTIONCALLMAP = 2,
+    FUNCTIONCALLINFO = 3,
+
+};
+
+}
+
+
+#endif
