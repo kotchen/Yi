93dca3a46df316fa0b3189ef78f1c9748d75184d
kotchen <549642010@qq.com>
2022-04-11 16:29:45 +0800

    单例模式

---
+++ util/Singleton.h
@@ -0,0 +1,40 @@
+﻿#ifndef __SINGLETON__
+#define __SINGLETON__
+namespace yi
+{
+    template <typename T>
+    class Singleton
+    {
+    private:
+        /* data */
+        static T **GetPPtr()
+        {
+            static T *ptr = nullptr;
+            return &ptr;
+        }
+
+    public:
+        static T *Get()
+        {
+            return *GetPPtr();
+        }
+
+        template <typename... Args>
+        static void New(Args &&...args)
+        {
+            *GetPPtr() = new T(std::forward<Args>(args)...);
+        }
+
+        static void Delete()
+        {
+            if (Get() != nullptr)
+            {
+                delete Get();
+                *GetPPtr() = nullptr;
+            }
+        }
+    };
+
+}
+
+#endif
