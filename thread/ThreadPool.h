9b3f1fc5a25a31e80f47ac144732b6807a0ba9c9
kotchen <549642010@qq.com>
2022-05-07 16:08:23 +0800

    修改编译Bug

--- thread/ThreadPool.h
+++ thread/ThreadPool.h
@@ -19,8 +19,56 @@ namespace thread
     class ThreadPool
     {
     public:
-        ThreadPool(size_t);
-        ~ThreadPool();
+        ThreadPool() = delete;
+        // the constructor just launches some amount of workers
+        inline ThreadPool(size_t threads)
+            : stop(false)
+        {
+            for (size_t i = 0; i < threads; ++i)
+                workers.emplace_back(
+                    [this]
+                    {
+                        for (;;)
+                        {
+                            std::function<void()> task;
+
+                            {
+                                std::unique_lock<std::mutex> lock(this->queue_mutex);
+                                this->condition.wait(lock,
+                                                     [this]
+                                                     { return this->stop || !this->tasks.empty(); });
+                                if (this->stop && this->tasks.empty())
+                                    return;
+                                task = std::move(this->tasks.front());
+                                this->tasks.pop();
+                            }
+
+                            try
+                            {
+                                task();
+                            }
+                            catch (std::exception &e)
+                            {
+                                std::cout << "running task, with exception" << e.what() << std::endl;
+                            }
+                        }
+                    });
+        }
+        // the destructor joins all threads
+        inline ~ThreadPool()
+        {
+            {
+                std::unique_lock<std::mutex> lock(queue_mutex);
+                stop = true;
+            }
+            condition.notify_all();
+            for (std::thread &worker : workers)
+            {
+                if (worker.joinable())
+                    worker.join();
+            }
+        }
+
         template <class F, class... Args>
         auto enqueue(F &&f, Args &&...args) -> std::future<typename std::result_of<F(Args...)>::type>
         {
@@ -43,7 +91,6 @@ namespace thread
             condition.notify_one();
             return res;
         }
-        void join();
 
     private:
         // need to keep track of threads so we can join them
